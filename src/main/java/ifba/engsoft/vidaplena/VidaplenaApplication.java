601e00116ac46d15b21c4f34dbc43ab77095f9f4
lufecrx <luizfelipecrx@gmail.com>
2026-07-04 18:33:17 -0300

    Initial commit

---
+++ src/main/java/ifba/engsoft/vidaplena/VidaplenaApplication.java
@@ -0,0 +1,13 @@
+package ifba.engsoft.vidaplena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidaplenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidaplenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifba/engsoft/vidaplena/VidaplenaApplicationTests.java
@@ -0,0 +1,13 @@
+package ifba.engsoft.vidaplena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaplenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
